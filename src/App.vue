e2ff848e4c545ac6f30866d6f25cf42e46b5bf58
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-21 13:18:01 +0100

    added imgs/ added HeaderComponent + base structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-
-  </div>
+  <HeaderComponent/>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue'
   export default {
+    components:{
+      HeaderComponent,
+    }
     
   }
 </script>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <div class="container py-4 d-flex justify-content-between ">
+            <div>
+                <img src="../assets/images/logo.png" alt="Futio">
+            </div>
+            <nav>
+                <ul class="d-flex column-gap-5 ">
+                    <li><a href="#" @clik.prevent>Home</a></li>
+                    <li><a href="#" @clik.prevent>Home</a></li>
+                    <li><a href="#" @clik.prevent>Home</a></li>
+
+                </ul>
+            </nav>
+            <div>
+                <button class="">Live Streaming</button>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        name:'HeaderComponent',
+
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
